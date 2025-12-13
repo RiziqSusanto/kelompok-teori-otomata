0b6e750e05090d5c18a52c1eb5d907a14200393c
Irvan Maulana <irvan@digi-id.id>
2025-12-13 09:24:24 +0700

    [Pertemuan-10|Irvan] feat : parser logic

---
+++ irvan/parser.js
@@ -0,0 +1,116 @@
+import { Lexer } from './lexer.js';
+
+const TOKEN = {
+    INT: "INT",
+    PLUS: "PLUS",
+    MINUS: "MINUS",
+    MUL: "MUL",
+    DIV: "DIV",
+    LPAREN: "LPAREN",
+    RPAREN: "RPAREN",
+    EOF: "EOF"
+};
+
+export class Parser {
+    constructor(input) {
+        this.lexer = new Lexer(input);
+        this.tokens = this.lexer.getTokens();
+        this.tokens.push({ type: TOKEN.EOF, value: null });
+        this.pos = 0;
+        this.currentToken = this.tokens[this.pos];
+    }
+
+    error() {
+        throw new Error(`Invalid`);
+    }
+
+    eat(tokenType) {
+        if (this.currentToken.type === tokenType) {
+            this.pos++;
+            this.currentToken = this.tokens[this.pos];
+        } else {
+            this.error(`Diharapkan ${tokenType}, tapi mendapat ${this.currentToken.type}`);
+        }
+    }
+
+    factor() {
+        const token = this.currentToken;
+
+        if (token.type === TOKEN.INT) {
+            this.eat(TOKEN.INT);
+            return token.value;
+        } else if (token.type === TOKEN.LPAREN) {
+            this.eat(TOKEN.LPAREN);
+            const result = this.expr();
+            this.eat(TOKEN.RPAREN);
+            return result;
+        } else {
+            this.error("Diharapkan integer atau ekspresi dalam kurung");
+        }
+    }
+
+    term() {
+        let result = this.factor();
+
+        while (this.currentToken.type === TOKEN.MUL || this.currentToken.type === TOKEN.DIV) {
+            const token = this.currentToken;
+            if (token.type === TOKEN.MUL) {
+                this.eat(TOKEN.MUL);
+                result = result * this.factor();
+            } else if (token.type === TOKEN.DIV) {
+                this.eat(TOKEN.DIV);
+                const divisor = this.factor();
+                if (divisor === 0) {
+                    this.error("Pembagian dengan nol tidak diperbolehkan");
+                }
+                result = result / divisor;
+            }
+        }
+
+        return result;
+    }
+
+    expr() {
+        let result = this.term();
+
+        while (this.currentToken.type === TOKEN.PLUS || this.currentToken.type === TOKEN.MINUS) {
+            const token = this.currentToken;
+            if (token.type === TOKEN.PLUS) {
+                this.eat(TOKEN.PLUS);
+                result = result + this.term();
+            } else if (token.type === TOKEN.MINUS) {
+                this.eat(TOKEN.MINUS);
+                result = result - this.term();
+            }
+        }
+
+        return result;
+    }
+
+    parse() {
+        try {
+            const result = this.expr();
+
+            if (this.currentToken.type !== TOKEN.EOF) {
+                this.error("Token tak terduga setelah ekspresi selesai");
+            }
+
+            return {
+                valid: true,
+                result: result,
+                message: "Ekspresi valid"
+            };
+        } catch (error) {
+            return {
+                valid: false,
+                result: null,
+                message: error.message
+            };
+        }
+    }
+
+    static validate(input) {
+        const parser = new Parser(input);
+        return parser.parse();
+    }
+}
